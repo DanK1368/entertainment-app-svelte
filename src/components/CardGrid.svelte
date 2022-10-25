4db3b5e1214be6c56db84d03b0c6f297bb2f1087
DanK1368 <danielkoett@gmail.com>
2022-10-25 09:41:58 +0200

    feat: increase gap and set max width on medium breakpoint

--- src/components/CardGrid.svelte
+++ src/components/CardGrid.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <h2 class="text-text text-2xl font-light ">{title}</h2>
-<section class=" customGridTemplate gap-4 pt-4 pb-6 ">
+<section class=" customGridTemplate gap-4 pt-4 pb-6 md:gap-7 lg:max-w-[90vw]">
 	<slot />
 </section>

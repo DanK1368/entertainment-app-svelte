5f0ba8188b8970de019b08e7c1cc74342c5ee916
DanK1368 <danielkoett@gmail.com>
2022-11-01 11:53:17 +0100

    fix: search takes user to correct url path

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -2,7 +2,6 @@
 	import { goto } from '$app/navigation';
 	import searchIcon from '../assets/icon-search.svg';
 	import { page } from '$app/stores';
-	console.log($page.url.pathname);
 
 	export let placeholder;
 	let searchValue = '';
@@ -16,9 +15,9 @@
 	// $: placeholder = handleInputPlaceholder($page.url.pathname);
 
 	const handleSearch = () => {
-		if ($page.url.pathname === '/movies' || $page.url.pathname === '/') {
+		if ($page.url.pathname.startsWith('/movies') || $page.url.pathname === '/') {
 			goto(`/movies/search/${searchValue}`);
-		} else if ($page.url.pathname === '/tvshows') {
+		} else if ($page.url.pathname.startsWith('/tvshows')) {
 			goto(`/tvshows/search/${searchValue}`);
 		}
 	};
